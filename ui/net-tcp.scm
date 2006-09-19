5a67b22e66e102eca0d402b6bc8d39213459e244
Stanislav Ievlev <inger@altlinux.org>
2006-09-19 16:29:05 +0400

    backend: split ip and netmask ui: added special netmask widget

--- ui/net-tcp.scm
+++ ui/net-tcp.scm
@@ -1,8 +1,17 @@
 (document:surround "/std/base")
 (document:insert "/std/functions")
 
+(document:envelop with-container-presentations ((netmask '/net-tcp/netmask text)) )
+
 width 600
 height 500
 
 
-(write (woo-list-names "/net-tcp"))
+
+(define iface (woo-read-first "/net-tcp/eth0"))
+
+
+(document:id i (edit (woo-get-option iface 'ip)))
+(document:id n (netmask (woo-get-option iface 'mask)))
+
+
