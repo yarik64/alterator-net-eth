8af9194b7e3ea876528763806672783fba360940
Stanislav Ievlev <inger@altlinux.org>
2006-11-23 17:22:15 +0300

    - use constraints automation instead of on-toggle callbacks

--- alterator-net-tcp/ui/net-tcp/index.scm
+++ alterator-net-tcp/ui/net-tcp/index.scm
@@ -16,30 +16,30 @@ margin 10
  (document:id ifaces (combobox layout-policy 20 -1)))
 
 
-(document:id iface-enabled (checkbox "Enabled"))
-(document:id iface-dhcp (checkbox "Use DHCP"))
+(document:id iface-enabled (checkbox "Enabled" widget-name "state"))
+(document:id iface-dhcp (checkbox "Use DHCP" widget-name "dhcp"))
 
 (hbox
  layout-policy 100 -1
  (label "IP address" layout-policy 30 -1)
- (document:id iface-ip (edit "" layout-policy -2 -1)))
+ (document:id iface-ip (edit "" layout-policy -2 -1 widget-name "ip")))
 
 (hbox
  layout-policy 100 -1
  (label "NetMask" layout-policy 30 -1)
- (document:id iface-mask (netmask "" layout-policy -2 -1)))
+ (document:id iface-mask (netmask "" layout-policy -2 -1 widget-name "mask")))
 
 (hbox
  layout-policy 100 -1
  (label "Default gateway" layout-policy 30 -1)
- (document:id iface-gw (edit "" layout-policy -2 -1)))
+ (document:id iface-gw (edit "" layout-policy -2 -1 widget-name "default")))
 
- (vertical-spacer)
- (hbox layout-policy 100 -1
-       spacing 10
-       (document:id c-button (button "Commit" layout-policy 33 -1))
-       (document:id r-button (button "Reset"  layout-policy 33 -1))
-       (document:id q-button (button "Quit" layout-policy -2 -1)))
+(vertical-spacer)
+(hbox layout-policy 100 -1
+      spacing 10
+      (document:id c-button (button "Commit" layout-policy 33 -1))
+      (document:id r-button (button "Reset"  layout-policy 33 -1))
+      (document:id q-button (button "Quit" layout-policy -2 -1)))
 
 
 
@@ -50,13 +50,6 @@ margin 10
          
          (iface-enabled state (woo-get-option cmd 'state #f))
          (iface-dhcp state (woo-get-option cmd 'dhcp #f))
-
-         (cond
-          ((not (iface-enabled state))
-           (iface-enabled toggled))
-          ((iface-dhcp state)
-           (iface-dhcp toggled)))
-         
          (iface-ip text (woo-get-option cmd 'ip))
          (iface-mask text (woo-get-option cmd 'mask))
          (iface-gw text (woo-get-option cmd 'default)))))
@@ -81,18 +74,9 @@ margin 10
 (r-button (when clicked (update-interface (ifaces text))))
 (q-button (when clicked (document:end)))
 
-
-(iface-enabled (when toggled
-                 ((widgets iface-dhcp
-                           iface-ip
-                           iface-mask
-                           iface-gw) activity (iface-enabled state))
-                 (and (iface-dhcp state) (iface-dhcp toggled))))
-
-(iface-dhcp (when toggled ((widgets iface-ip
-                                    iface-mask
-                                    iface-gw) activity (not (iface-dhcp state)))))
-                 
-
 ;;init first update
 (ifaces current 0 selected)
+
+(document:root
+ (when loaded
+   (update-constraints "write" "/net-tcp")))
