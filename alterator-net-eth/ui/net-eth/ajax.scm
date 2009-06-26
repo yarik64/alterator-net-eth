02c4f532453b1df7bc9c811a9b5b567dff91deed
Stanislav Ievlev <inger@altlinux.org>
2009-06-26 17:00:00 +0400

    - use 'none' workflow

--- alterator-net-eth/ui/net-eth/ajax.scm
+++ alterator-net-eth/ui/net-eth/ajax.scm
@@ -3,6 +3,8 @@
     :use-module (alterator woo)
     :export (init))
 
+;;; low level
+
 (define (update-configuration configuration)
     (form-update-activity
       '("ip" "mask" "default")
@@ -31,6 +33,7 @@
 			    "computer_name" "dns" "search"
 			    "ip" "mask" "default" "configuration"))))
 
+;;; high level
 
 (define (update-interface)
   (or (catch/message
@@ -44,10 +47,37 @@
 (define (advanced-interface)
   (form-replace (format #f "/net-eth/advanced?iface=~A" (form-value "name"))))
 
+(define (commit-interface)
+  (catch/message
+    (lambda()
+      (write-interface (or (form-value "name") ""))
+      (woo-write "/net-eth" 'commit #t))))
+
+(define (reset-interface)
+  (catch/message
+    (lambda()
+      (woo-write "/net-eth" 'reset #t)
+
+      (read-interface (or (form-value "iface") ""))
+      (form-update-value "prev_name" (or (form-value "iface") "")))))
+
+(define (init-interface)
+  (catch/message
+    (lambda()
+      (form-update-enum "mask" (woo-list "/net-eth/avail_masks" 'language (form-value "language")))
+      (form-update-enum "configuration" (woo-list "/net-eth/avail_configurations" 'language (form-value "language")))
+      (form-update-enum "name" (woo-list "/net-eth/avail_ifaces" 'language (form-value "language")))
+
+      (read-interface (or (form-value "iface") ""))
+      (form-update-value "prev_name" (or (form-value "iface") "")))))
+
+
 (define (init)
- (read-interface (or (form-value "iface") ""))
- (form-update-value "prev_name" (form-value "name"))
+ (init-interface)
 
  (form-bind "name" "change" update-interface)
  (form-bind "configuration" "change" (lambda() (update-configuration (form-value "configuration"))))
- (form-bind "advanced" "click" advanced-interface))
+ (form-bind "advanced" "click" advanced-interface)
+
+ (form-bind "commit" "click" commit-interface)
+ (form-bind "reset" "click" reset-interface))
